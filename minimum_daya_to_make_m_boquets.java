dff020e40166c8651cb6091bbe90c9cc57b8631d
Shreeprabha N <96766072+shreeprabha1217@users.noreply.github.com>
2024-09-06 14:46:13 +0530

    Create minimum_daya_to_make_m_boquets.java

---
+++ minimum_daya_to_make_m_boquets.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean possible(int[] bloomDay,int mid,int m,int k){
+        int count=0;
+        int no_of_bloom=0;
+        for(int i=0;i<bloomDay.length;i++){
+            if(bloomDay[i]<=mid){
+                count++;
+            }else{
+                no_of_bloom+=(count/k);
+                count=0;
+            }
+        }
+        no_of_bloom+=(count/k);
+        
+        return no_of_bloom>=m;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        long val = (long) m * k;
+        int n = bloomDay.length; 
+        if (val > n) return -1; 
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, bloomDay[i]);
+            maxi = Math.max(maxi, bloomDay[i]);
+        }
+
+        
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(bloomDay, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
